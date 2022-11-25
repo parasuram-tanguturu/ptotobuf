768ac6ea196c2ccb2c0917d3adb0dea14c80ddb0
Parasuram <Parasuram.Tanguturu@ruckuswireless.com>
2022-11-25 11:50:34 +0530

    . Proto - Serialization & Deserialization

--- src/main/java/com/parasuram/protobuf/PersonDemo.java
+++ src/main/java/com/parasuram/protobuf/PersonDemo.java
@@ -2,20 +2,27 @@ package com.parasuram.protobuf;
 
 import com.parasuram.models.Person;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class PersonDemo{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Person ram1 = Person.newBuilder()
                 .setName("ram")
                 .setAge(32)
                 .build();
-        Person ram2 = Person.newBuilder()
-                .setName("ram")
-                .setAge(32)
-                .build();
         System.out.println(ram1.toString());
-        System.out.println(ram1.equals(ram2));
-        //content comparision happens by default
+
+        Path path=Paths.get("ram.serFile");
+        Files.write(path,ram1.toByteArray());
+
+        byte[] bytes = Files.readAllBytes(path);
+        Person  ramDeserilzed = Person.parseFrom(bytes);
+        System.out.println(ramDeserilzed.toString());
+
     }
 }
