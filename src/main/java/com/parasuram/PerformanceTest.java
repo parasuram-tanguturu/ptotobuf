49bcf86c1843cf82e120b66ae469bfa9d5c0035b
Parasuram <Parasuram.Tanguturu@ruckuswireless.com>
2022-11-25 15:21:58 +0530

    . Proto vs JSON - Performance Comparison - Part - 2

--- src/main/java/com/parasuram/PerformanceTest.java
+++ src/main/java/com/parasuram/PerformanceTest.java
@@ -1,21 +1,58 @@
 package com.parasuram;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.parasuram.json.JPerson;
+import com.parasuram.models.Person;
+
+import java.io.IOException;
+
 public class PerformanceTest {
 
-    //jsom
-    Runnable runnable1;
+    public static void main(String[] args) {
+        //jsom
+        JPerson person=new JPerson();
+        person.setName("ram");
+        person.setAge(32);
+        ObjectMapper mapper=new ObjectMapper();
 
-    //protobuf
-    Runnable runnable2;
-}
-    private static void runPeformaceTest(Runnable runnable,String method){
-        long time1=System.currentTimeMillis();
-        for (int i=1;i<1_000_000;i++){
-            runnable.run();
+        Runnable runnable1=()->{
+            try {
+                byte[] bytes = mapper.writeValueAsBytes(person);
+                JPerson person1 = mapper.readValue(bytes, JPerson.class);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        long time2=System.currentTimeMillis();
-        System.out.println(
-                method+" : "+(time2-time1)+"ms"
-        );
+
+
+        //protobuf
+        Person ram = Person.newBuilder()
+                .setName("ram")
+                .setAge(32)
+                .build();
+
+        Runnable runnable2=()->{
+            try {
+                byte[] bytes = ram.toByteArray();
+                Person.parseFrom(bytes);
+            } catch (InvalidProtocolBufferException e) {
+                e.printStackTrace();
+            }
+        };
+    }
+    
+    private static void runPeformaceTest(Runnable runnable,String method){
+            long time1=System.currentTimeMillis();
+            for (int i=1;i<1_000_000;i++){
+                runnable.run();
+            }
+            long time2=System.currentTimeMillis();
+            System.out.println(
+                    method+" : "+(time2-time1)+"ms"
+            );
     }
 }
