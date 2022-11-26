e57d72339f4f6fac760a0506256c360f4acbbc5f
Parasuram <Parasuram.Tanguturu@ruckuswireless.com>
2022-11-26 13:57:24 +0530

    23. Proto - Composition Demo--add ,addAll

--- src/main/java/com/parasuram/ComposistionDemo.java
+++ src/main/java/com/parasuram/ComposistionDemo.java
@@ -4,6 +4,10 @@ import com.parasuram.models.Address;
 import com.parasuram.models.Car;
 import com.parasuram.models.Person;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ComposistionDemo {
     public static void main(String[] args) {
         Address address=Address.newBuilder()
@@ -11,18 +15,35 @@ public class ComposistionDemo {
                 .setCity("BLR")
                 .setStreet("ECITY")
                 .build();
-        Car car= Car.newBuilder()
+        Car car1= Car.newBuilder()
                 .setMake("honda")
                 .setModel("accord")
                 .setYear(2020)
+                .build();
+        Car car2= Car.newBuilder()
+                .setMake("honda")
+                .setModel("civic")
+                .setYear(2005)
                 .build();
         Person person=Person.newBuilder()
                 .setName("ram")
                 .setAge(31)
                 .setAddress(address)
-                .setCar(car)
+                .addCar(car1)
+                .addCar(car2)
                 .build();
 
         System.out.println(person);
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        Person person2=Person.newBuilder()
+                .setName("ram")
+                .setAge(31)
+                .setAddress(address)
+                .addAllCar(cars)
+                .build();
+        System.out.println(person2);
     }
 }
