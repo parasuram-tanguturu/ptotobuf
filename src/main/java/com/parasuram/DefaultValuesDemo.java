f505a0591685055ea29d55eec567a1e1f27b066b
Parasuram <Parasuram.Tanguturu@ruckuswireless.com>
2022-11-26 16:08:38 +0530

    27. Proto - Default Values

---
+++ src/main/java/com/parasuram/DefaultValuesDemo.java
@@ -0,0 +1,14 @@
+package com.parasuram;
+
+import com.parasuram.models.Person;
+
+public class DefaultValuesDemo {
+    public static void main(String[] args) {
+        Person person=Person.newBuilder().build();
+
+        System.out.println("City is"+ person.getAddress().getCity());
+
+        //has method will be generated for composite object
+        System.out.println(person.hasAddress());
+    }
+}
