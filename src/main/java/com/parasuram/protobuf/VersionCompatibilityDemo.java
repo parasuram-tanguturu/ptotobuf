75f3d7b8234d25d42db5392a8bf3c85f59cba3fb
Parasuram <Parasuram.Tanguturu@ruckuswireless.com>
2022-11-27 06:22:06 +0530

    33. Proto - API/Message Changes - Part - 2 model is not there so field will come as 2 for v1 also.

--- src/main/java/com/parasuram/protobuf/VersionCompatibilityDemo.java
+++ src/main/java/com/parasuram/protobuf/VersionCompatibilityDemo.java
@@ -21,7 +21,7 @@ public class VersionCompatibilityDemo {
                         .build();
 
            Files.write(pathV1,television.toByteArray());*/
-        System.out.println(Television.parseFrom(Files.readAllBytes(pathV2)));
+        System.out.println(Television.parseFrom(Files.readAllBytes(pathV1)));
 
        /* Television television=Television.newBuilder()
                 .setBrand("sony")
